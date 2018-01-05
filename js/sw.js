1f21cc0dbc5751bccd6f7b65b0226e61cfc78674
Ziwa Mukungu <ziwabigboy@gmail.com>
2018-01-05 15:13:16 -0500

    service worker has error handling

--- js/sw.js
+++ js/sw.js
@@ -12,5 +12,14 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-	event.respondWith(cache.match(event.request));
+	event.respondWith(
+    cache.match(event.request).then(function(response) {
+      return response? response:fetch(event.request).then(function(res) {
+        return res.status == 404? new Response('404, not found'): res;
+      })
+      .catch(function() {
+        return new Response('site failed to load');
+      })
+    })
+  );
 });
