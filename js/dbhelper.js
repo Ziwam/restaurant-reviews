4eb574c4f6609562c0ac7efb4e439a833695b144
Ziwa Mukungu <ziwabigboy@gmail.com>
2018-01-08 01:12:56 -0500

    added alt text to images

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -154,6 +154,13 @@ class DBHelper {
   }
 
   /**
+   * Restaurant image Alt.
+   */
+  static imageAltForRestaurant(restaurant) {
+    return restaurant.alt;
+  }
+
+  /**
    * Map marker for a restaurant.
    */
   static mapMarkerForRestaurant(restaurant, map) {
